3980279dfd0d3d73d6b3a9a4d4d4fc117611d838
GraceSchwar <grace.schwar@gmail.com>
2020-09-09 14:52:47 -0400

    Upload

---
+++ Script.js
@@ -0,0 +1,126 @@
+
+//Store all books in an array
+let myLibrary = [];
+let newBook;
+const library = document.querySelector('#Library-container');
+const newBookBtn = document.querySelector('#newBtn');
+const addBtn = document.querySelector('#addBtn');
+const popUpForm = document.getElementById('popUp');
+const closePopUp = document.getElementsByTagName('span')[0];
+const form = document.getElementById('form');
+
+//button event listeners for create new book, add new book to page, close popup
+addBtn.addEventListener('click', addBookToLibrary);
+newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
+
+//Book Constructor
+function Book(title, author, pages, read) {
+    this.title = form.title.value; 
+    this.author = form.author.value; 
+    this.pages = form.pages.value + 'pg'; 
+    this.read = form.read.checked; 
+}
+
+//creates book from Book Constructor, adds to library
+function addBookToLibrary() {
+    event.preventDefault();
+    popUpForm.style.display = 'none';
+
+    newBook = new Book(title, author, pages,read); 
+    myLibrary.push(newBook); 
+    setData();  //saves updated array in local storage
+    render(); 
+    form.reset();
+}
+
+//Creates book visual in browser
+function render() {
+    const display = document.getElementById('Library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+   
+    for (let i=0; i<myLibrary.length; i++){
+        createBook(myLibrary[i]);
+    }
+}
+
+//creats book DOM elements, to use in render();
+function createBook(item) {
+
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const pageDiv = document.createElement('div');
+    const removeBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
+    
+    
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
+
+    authDiv.textContent = item.author;
+    authDiv.classList.add('author');
+    bookDiv.appendChild(authDiv);
+
+    pageDiv.textContent = item.pages;
+    pageDiv.classList.add('pages');
+    bookDiv.appendChild(pageDiv);
+
+
+    
+    readBtn.setAttribute('id', 'readBtn'+myLibrary.indexOf(item));
+    readBtn.classList.add('readBtn')
+    
+    bookDiv.appendChild(readBtn);
+    if(item.read===false) {
+        readBtn.textContent = 'Not Read';
+        readBtn.style.backgroundColor = '#e04f63';
+    }else {
+        readBtn.textContent = 'Read';
+        readBtn.style.backgroundColor = '#63da63'
+    }
+
+    removeBtn.textContent = 'Remove'; 
+    removeBtn.setAttribute('id', 'removeBtn');
+    bookDiv.appendChild(removeBtn);
+    
+    library.appendChild(bookDiv);
+
+    removeBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(item),1);
+        setData()
+        render();
+    });
+
+    //add toggle ability to each book 'read' button on click
+    readBtn.addEventListener('click', () => { 
+        item.read = !item.read; 
+        setData(); 
+        render();
+    }); 
+};
+
+// setting Library to be stored in local storage
+function setData() {
+    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+}
+
+//pulls books from local storage when page is refreshed
+function restore() {
+    if(!localStorage.myLibrary) {
+        render();
+    }else {
+        let objects = localStorage.getItem('myLibrary') // gets information from local storage to use in below loop to create DOM/display
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+        render();
+    }
+}
+
+restore();
+
